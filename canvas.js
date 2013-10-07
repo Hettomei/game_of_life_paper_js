851d1ab35d3489b73648412a61c3f4e80dc89d40
Hettomei <itsumo.sibyllin@gmail.com>
2013-10-07 15:31:19 +0200

    first random art lines

--- canvas.js
+++ canvas.js
@@ -2,5 +2,15 @@ $( document ).ready(function() {
   $('body').append('<p>Dom loaded</p>');
 
   var c = document.getElementById("mon_canvas");
-  var context = c.getContext("2d");
+  var ctx = c.getContext("2d");
+
+  ctx.beginPath();      // Début du chemin
+  ctx.moveTo(50,50);    // Le tracé part du point 50,50
+  ctx.lineTo(200,200);  // Un segment est ajouté vers 200,200
+  ctx.lineTo(100,200);  // Un segment est ajouté vers 200,200
+  ctx.lineTo(100,100);  // Un segment est ajouté vers 200,200
+  ctx.closePath();      // Fermeture du chemin (facultative)
+
+  ctx.lineWidth = 5;
+  ctx.stroke();
 });
